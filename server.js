1f246d4405545d0326848933e4b415c6a7c899e7
Roberto <robertoduran.0097@gmail.com>
2020-09-16 20:50:48 -0400

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(_dirname + 'dist/mtz'));
-app.get('/*', function(req,res){
-	res.sendFile(path.join(_dirname+'dist/mtz/index.html'));
+app.use(express.static(__dirname + '/mtz'));
+app.get('/*', function(req,res) {
+	res.sendFile(path.join(__dirname+'/mtz/index.html'));
 });
-app.listen(process.env.PORT|| 8080);
+app.listen(process.env.PORT || 8080);
